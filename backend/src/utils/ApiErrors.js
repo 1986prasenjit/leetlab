e6787d059932f4037757b4a2a0e940c03e9259c7
1986prasenjit <prasenjitbasu44@gmail.com>
2025-04-24 14:34:12 +0530

    added custom ApiErrors and ApiResponse

---
+++ backend/src/utils/ApiErrors.js
@@ -0,0 +1,17 @@
+class ApiErrors extends Error {
+  constructor(statusCode, message = "Something went Wrong", stack, error = []) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = null;
+    this.error = error;
+    this.success = false;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiErrors };
